a5c7b6fd98d1c976bf33a79253561eece8c1e266
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-07-03 00:01:54 +0200

    add Building Entity, Repo, Controller and Service

--- src/main/java/com/telran/libraryapp/controller/BuildingController.java
+++ src/main/java/com/telran/libraryapp/controller/BuildingController.java
@@ -1,4 +1,86 @@
 package com.telran.libraryapp.controller;
 
+import com.telran.libraryapp.entity.Building;
+import com.telran.libraryapp.service.BuildingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@RestController
+@RequestMapping("/buildings")
 public class BuildingController {
+
+    private final BuildingService service;
+
+    public BuildingController(BuildingService service) {
+        this.service = service;
+    }
+
+
+    @GetMapping
+    public List<Building> getAll() {
+        return service.getAll();
+    }
+
+
+    @GetMapping("/searchById")
+    public ResponseEntity<Building> getBildById(@RequestParam Integer id) {
+        Optional<Building> building = service.getBuildingById(id);
+        if (building.isPresent()) {
+            return new ResponseEntity<>(building.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Building> addBuilding(@RequestBody Building building) {
+        service.addBuilding(building);
+        return new ResponseEntity<>(building, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping
+    public ResponseEntity<Building> updateBuilding(@RequestBody Building building) {
+        boolean isUpdated = service.updateBuilding(building);
+        if (isUpdated) {
+            return new ResponseEntity<>(building, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(building, HttpStatus.CREATED);
+        }
+
+    }
+
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteById(@RequestParam Integer id) {
+        service.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+
+    @GetMapping("/serchByName")
+    public ResponseEntity<List<Building>> getBuildingByName(@RequestParam String name) {
+        List<Building> buildings = service.getBuildingByName(name);
+        if (!buildings.isEmpty()) {
+            return new ResponseEntity<>(buildings, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(Collections.EMPTY_LIST, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/deleteAllZipCodes")
+    public ResponseEntity<?> deleteAllZipCodes() {
+        service.deleteAllZipCodes();
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+
 }
