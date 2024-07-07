260f0a6c10474a5a436af72581d8b6125669f7ea
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-07-07 20:31:53 +0200

    updated author information for a new task

--- src/main/java/com/telran/libraryapp/repository/AuthorRepository.java
+++ src/main/java/com/telran/libraryapp/repository/AuthorRepository.java
@@ -1,13 +1,18 @@
 package com.telran.libraryapp.repository;
 
 import com.telran.libraryapp.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class AuthorRepository {
-    public List<Author> getAllAuthors() {
-        return Author.authorList;
-    }
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    List<Author> findAuthorByNameAndSurname(String name, String surname);
+    List<Author> findAuthorByName(String name);
+    List<Author> findAuthorBySurname(String surname);
+
+    @Query(value = "SELECT * FROM author a WHERE a.name LIKE ?1 OR a.surname LIKE ?1 OR a.author_info LIKE ?1% ", nativeQuery = true)
+    List<Author> findAuthorByRandomWord(String word);
 }
