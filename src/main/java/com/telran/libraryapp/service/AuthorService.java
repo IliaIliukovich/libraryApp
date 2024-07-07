404d58d697025752a911799927c1280df27dbfe3
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-07-07 20:31:53 +0200

    updated author information for a new task

--- src/main/java/com/telran/libraryapp/service/AuthorService.java
+++ src/main/java/com/telran/libraryapp/service/AuthorService.java
@@ -3,6 +3,7 @@ package com.telran.libraryapp.service;
 import com.telran.libraryapp.entity.Author;
 import com.telran.libraryapp.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,48 +13,49 @@ import java.util.Optional;
 public class AuthorService {
 
     private final AuthorRepository authorRepository;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Autowired
-    public AuthorService(AuthorRepository authorRepository) {
+    public AuthorService(AuthorRepository authorRepository, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.authorRepository = authorRepository;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     public List<Author> getAll(){
-        return authorRepository.getAllAuthors();
+        return authorRepository.findAll();
     }
     public Optional<Author> getNameAuthorByID(int id) {
-        return authorRepository.getAllAuthors().stream().filter(author -> author.getId() == id).findFirst();
+        return authorRepository.findById(id);
     }
     public void add(Author author) {
-        authorRepository.getAllAuthors().add(author);
+        authorRepository.save(author);
     }
     public boolean updateAuthor(Author author) {
-        if (authorRepository.getAllAuthors().contains(author)) {
-            int index = authorRepository.getAllAuthors().indexOf(author);
-            authorRepository.getAllAuthors().set(index, author);
+        Optional<Author> authorOptional = authorRepository.findById(author.getId());
+        if (authorOptional.isPresent()){
+            authorRepository.save(author);
             return true;
-        } else {
-            authorRepository.getAllAuthors().add(author);
+        }else{
+            authorRepository.save(author);
             return false;
         }
     }
-    public  void  removeAuthor(int id){
-        authorRepository.getAllAuthors().removeIf(author1 -> author1.getId() == id);
+    public void removeAuthor(int id){
+        authorRepository.deleteById(id);
     }
     public List<Author> returnAuthorByNameOrSurname(String name, String surname){
         if (name != null && surname != null) {
-            return authorRepository.getAllAuthors().stream().filter(author -> author.getSurname().startsWith(surname) && author.getName().startsWith(name)).toList();
+          return authorRepository.findAuthorByNameAndSurname(name,surname);
         } else if (name != null) {
-            return authorRepository.getAllAuthors().stream().filter(author -> author.getName().startsWith(name)).toList();
+            return authorRepository.findAuthorByName(name);
         } else if (surname != null) {
-            return authorRepository.getAllAuthors().stream().filter(author -> author.getSurname().startsWith(surname)).toList();
+            return authorRepository.findAuthorBySurname(surname);
         } else {
-            return authorRepository.getAllAuthors();
+            return authorRepository.findAll();
         }
     }
     public List<Author> getAuthorByRandomWord(String randomWord){
-        return authorRepository.getAllAuthors().stream().filter(author -> author.getName().contains(randomWord)
-                || author.getSurname().contains(randomWord)
-                || author.getAuthorInfo().contains(randomWord)).toList();
+        String formattedWord = '%' + randomWord.trim() + '%';
+        return authorRepository.findAuthorByRandomWord(formattedWord);
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,16 @@ insert into book (title, author, category, available_amount, isbn)
     values ('Java in Action1', 'Author', 'Java', 3, '1'),
            ('Java in Action2', 'Author', 'Java', 3, '2'),
            ('Java in Action3', 'Author', 'Java', 3, '3');
+insert into author(id, name, surname, author_info)
+values (1, 'William', 'Shakespeare',
+        'English playwright, poet, and actor, widely regarded as the greatest writer in the English language.'),
+       (2, 'Jane', 'Austen',
+        'English novelist known primarily for her six major novels, which interpret, critique and comment upon the British landed gentry at the end of the 18th century.'),
+       (3, 'Charles', 'Dickens',
+        'English writer and social critic. He created some of the worlds best-known fictional characters and is regarded by many as the greatest novelist of the Victorian era.'),
+       (4, 'George', 'Orwell',
+        'English novelist, essayist, journalist and critic, whose work is marked by lucid prose, awareness of social injustice, opposition to totalitarianism, and outspoken support of democratic socialism.'),
+       (5, 'Virginia', 'Woolf',
+        'English writer, considered one of the most important modernist 20th-century authors and also a pioneer in the use of stream of consciousness as a narrative device.'),
+       (6, 'Agatha', 'Christie',
+        'English writer known for her sixty-six detective novels and fourteen short story collections, particularly those revolving around her fictional detectives Hercule Poirot and Miss Marple.');
