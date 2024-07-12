a849a39eff1cc88fa86bb7e086b4615258c613ff
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-07-12 12:43:28 +0200

    update validation building

--- src/main/java/com/telran/libraryapp/controller/BuildingController.java
+++ src/main/java/com/telran/libraryapp/controller/BuildingController.java
@@ -6,13 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/buildings")
