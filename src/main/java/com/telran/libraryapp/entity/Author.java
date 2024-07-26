319152adad4383fa8c3d35dae9c818509f046790
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-07-26 15:08:09 +0200

    fixed class Author

--- src/main/java/com/telran/libraryapp/entity/Author.java
+++ src/main/java/com/telran/libraryapp/entity/Author.java
@@ -48,7 +48,6 @@ public class Author {
         Author author = (Author) o;
         return Objects.equals(id, author.id);
     }
-
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
