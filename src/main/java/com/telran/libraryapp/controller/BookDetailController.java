645472e9cda6655e054683e0283ecc323449b906
VoytovychM <mary.voytovych@gmail.com>
2024-08-01 16:02:16 +0200

    add loggers to the BookDetail Controller and Service

--- src/main/java/com/telran/libraryapp/controller/BookDetailController.java
+++ src/main/java/com/telran/libraryapp/controller/BookDetailController.java
@@ -4,9 +4,11 @@ package com.telran.libraryapp.controller;
 import com.telran.libraryapp.entity.BookDetail;
 import com.telran.libraryapp.service.BookDetailService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +16,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/bookDetails")
+@Validated
+@Slf4j
 public class BookDetailController {
 
     private final BookDetailService service;
@@ -25,6 +29,7 @@ public class BookDetailController {
 
     @GetMapping
     public List<BookDetail> getAll() {
+        log.info("Get all book details");
         return service.getAll();
     }
 
@@ -32,8 +37,10 @@ public class BookDetailController {
     public ResponseEntity<BookDetail> getBookDetailById(@PathVariable Long id) {
         Optional<BookDetail> bookDetail = service.getBDById(id);
         if (bookDetail.isPresent()) {
+            log.info("Get book detail by id: {} successfully ", id);
             return new ResponseEntity<>(bookDetail.get(), HttpStatus.OK);
         } else {
+            log.info("Get book detail by id: {} not found ", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -41,6 +48,7 @@ public class BookDetailController {
     @PostMapping("/addBookDetail")
     public ResponseEntity<BookDetail> addBookDetail(@RequestBody @Valid BookDetail bookDetail, @RequestParam Long bookID) {
         BookDetail savedBookDetail = service.add(bookDetail, bookID);
+        log.info("BookDetail with id = {} created", savedBookDetail.getId());
         return new ResponseEntity<>(savedBookDetail, HttpStatus.CREATED);
     }
 
@@ -48,8 +56,11 @@ public class BookDetailController {
     public ResponseEntity<BookDetail> updateBookDetail(@RequestBody @Valid BookDetail bookDetail) {
         boolean isUpdated = service.updateBookDetail(bookDetail);
         if (isUpdated) {
+            log.info("BookDetail with id = {} updated", bookDetail.getId());
+            log.debug("BookDetail with id = {} updated, publisher = {}, year = {}, abstractToBook = {}", bookDetail.getId(), bookDetail.getPublisher(), bookDetail.getYear(), bookDetail.getAbstractToBook());
             return new ResponseEntity<>(bookDetail, HttpStatus.OK);
         } else {
+            log.info("BookDetail with id = {} not found", bookDetail.getId());
             return new ResponseEntity<>(bookDetail, HttpStatus.NOT_FOUND);
         }
     }
@@ -57,6 +68,7 @@ public class BookDetailController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteBookDetail(@PathVariable Long id) {
         service.remove(id);
+        log.info("BookDetail with id = {} deleted", id);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/telran/libraryapp/service/BookDetailService.java
+++ src/main/java/com/telran/libraryapp/service/BookDetailService.java
@@ -4,6 +4,8 @@ import com.telran.libraryapp.entity.Book;
 import com.telran.libraryapp.entity.BookDetail;
 import com.telran.libraryapp.repository.BookDetailRepository;
 import com.telran.libraryapp.repository.BookRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,9 @@ import java.util.Optional;
 
 @Service
 public class BookDetailService {
+
+    private static final Logger logger = LogManager.getLogger(BookDetailService.class);
+
     private final BookDetailRepository bookDetailRepository;
     private final BookRepository bookRepository;
 
@@ -23,12 +28,16 @@ public class BookDetailService {
     }
 
     public List<BookDetail> getAll() {
-        return bookDetailRepository.findAll();
+        List<BookDetail> bookDetails = bookDetailRepository.findAll();
+        logger.debug("BookDetails retrieved from DB: {}", () -> bookDetails.stream().map(BookDetail::getPublisher).toList());
+        return bookDetails;
     }
 
 
     public Optional<BookDetail> getBDById(Long id) {
-        return bookDetailRepository.findById(id);
+        Optional<BookDetail> bookDetail = bookDetailRepository.findById(id);
+        logger.debug("BookDetail retrieved from DB: id =  {}", () -> bookDetail.orElse(null));
+        return bookDetail;
 
     }
 
@@ -36,13 +45,16 @@ public class BookDetailService {
     public BookDetail add(BookDetail bookDetail, Long bookID) {
         Optional<Book> optional = bookRepository.findById(bookID);
         if (optional.isPresent()) {
+            logger.debug("Book exists in DB: {}", bookID);
             BookDetail savedBookDetail = bookDetailRepository.save(bookDetail);
             Book book = optional.get();
             BookDetail old = book.getBookDetail();
             book.setBookDetail(savedBookDetail);
             bookRepository.save(book);
+            logger.debug("BookDetail added to Book from DB: {}", () -> savedBookDetail);
             if (old != null) {
                 bookDetailRepository.deleteById(old.getId());
+                logger.debug("BookDetail deleted from DB: {}", () -> savedBookDetail);
             }
             return savedBookDetail;
         }
@@ -53,8 +65,10 @@ public class BookDetailService {
         Optional<BookDetail> optional = bookDetailRepository.findById(bookDetail.getId());
         if (optional.isPresent()) {
             bookDetailRepository.save(bookDetail);
+            logger.debug("BookDetail updated from DB: id =  {}", bookDetail.getId());
             return true;
         } else {
+            logger.error("BookDetail not found : id = {}", bookDetail.getId());
             return false;
         }
     }
@@ -67,7 +81,10 @@ public class BookDetailService {
             Book book = optional.get();
             book.setBookDetail(null);
             bookRepository.save(book);
+            logger.info("BookDetail to the Book {} removed from DB", bookDetail);
+
         }
         bookDetailRepository.deleteById(id);
+        logger.debug("BookDetail removed from DB: {}", () -> bookDetail);
     }
 }
