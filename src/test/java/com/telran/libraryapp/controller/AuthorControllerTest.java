a2a088bb55522312a70c2fb3f3780eb6b19192e7
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-08-02 10:07:03 +0200

    updated AuthorRepositoryTest, AuthorServiceTest, AuthorControllerTest

---
+++ src/test/java/com/telran/libraryapp/controller/AuthorControllerTest.java
@@ -0,0 +1,72 @@
+package com.telran.libraryapp.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.telran.libraryapp.entity.Author;
+import com.telran.libraryapp.service.AuthorService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthorController.class)
+class AuthorControllerTest {
+
+    @MockBean
+    private AuthorService service;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getAuthors() throws Exception {
+        mockMvc.perform(get("/authors").contentType("application/json"))
+                .andExpect(status().isOk());
+        verify(service).getAll();
+    }
+
+    @Test
+    void getAuthorById() throws Exception {
+        Author author = new Author();
+        author.setId(15L);
+
+        when(service.getNameAuthorByID(15L)).thenReturn(Optional.of(author));
+        mockMvc.perform(get("/authors/15").contentType("application/json"))
+                .andExpect(status().isOk());
+
+        when(service.getNameAuthorByID(178L)).thenReturn(Optional.empty());
+        mockMvc.perform(get("/authors/178").contentType("application/json"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void addAuthor() throws Exception {
+        Author author = new Author();
+        author.setId(12L);
+        author.setName("Jane");
+        author.setSurname("Done");
+        Mockito.doNothing().when(service).add(Mockito.any(Author.class));
+        mockMvc.perform(post("/authors")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(author)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(12L))
+                .andExpect(jsonPath("$.name").value("Jane"))
+                .andExpect(jsonPath("$.surname").value("Done"));
+
+        verify(service).add(Mockito.any(Author.class));
+
+    }
+
+}
---
+++ src/test/java/com/telran/libraryapp/service/AuthorServiceTest.java
@@ -0,0 +1,102 @@
+package com.telran.libraryapp.service;
+
+import com.telran.libraryapp.entity.Author;
+import com.telran.libraryapp.repository.AuthorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorServiceTest {
+    private static AuthorService authorService;
+    private static AuthorRepository authorRepository;
+
+    @BeforeEach
+    public void init() {
+        authorRepository = Mockito.mock(AuthorRepository.class);
+        authorService = new AuthorService(authorRepository);
+    }
+
+    @Test
+    void getAll() {
+        authorService.getAll();
+        Mockito.verify(authorRepository).findAll();
+    }
+
+    @Test
+    void getNameAuthorByID() {
+        Long id = 2L;
+        authorService.getNameAuthorByID(id);
+        Mockito.verify(authorRepository).findById(id);
+    }
+
+    @Test
+    public void add() {
+        Author author = new Author();
+        author.setId(156L);
+        author.setName("Agata");
+        authorService.add(author);
+        Mockito.verify(authorRepository).save(author);
+    }
+
+    @Test
+    public void updateAuthor() {
+        Author author = new Author();
+        author.setName("New name author");
+        author.setId(123L);
+
+        Author oldAuthor = new Author();
+        oldAuthor.setName("Old name author");
+        oldAuthor.setId(123L);
+
+        Mockito.when(authorRepository.findById(author.getId())).thenReturn(Optional.of(oldAuthor));
+        Mockito.when(authorRepository.save(author)).thenReturn(author);
+
+        boolean result = authorService.updateAuthor(author);
+
+        Mockito.verify(authorRepository).findById(author.getId());
+        Mockito.verify(authorRepository).save(author);
+        assertTrue(result);
+
+        author = new Author();
+        author.setName("New name author");
+        author.setId(777L);
+
+        Mockito.when(authorRepository.findById(777L)).thenReturn(Optional.empty());
+        result = authorService.updateAuthor(author);
+        assertFalse(result);
+    }
+
+    @Test
+    public void removeAuthor() {
+        Long id = 1l;
+        authorService.removeAuthor(id);
+        Mockito.verify(authorRepository).deleteById(id);
+    }
+
+    @Test
+    void returnAuthorByNameOrSurname() {
+        authorService.returnAuthorByNameOrSurname("Dave", "Big");
+        Mockito.verify(authorRepository).findAuthorByNameAndSurname("Dave","Big");
+
+        authorService.returnAuthorByNameOrSurname("Daniel", null);
+        Mockito.verify(authorRepository).findAuthorByName("Daniel");
+
+        authorService.returnAuthorByNameOrSurname(null, "Rowling");
+        Mockito.verify(authorRepository).findAuthorBySurname("Rowling");
+
+        authorService.returnAuthorByNameOrSurname(null, null);
+        Mockito.verify(authorRepository).findAll();
+
+    }
+
+    @Test
+    void getAuthorByRandomWord() {
+        authorService.getAuthorByRandomWord("Jane");
+        Mockito.verify(authorRepository).findAuthorByRandomWord("%Jane%");
+    }
+}
